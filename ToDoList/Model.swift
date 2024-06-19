dbf38fb0af271317a817f6ff8bcbf0da0c2006d8
Dimash <dimashaijarikov@gmail.com>
2024-06-19 21:10:10 +0500

    updated

--- ToDoList/Model.swift
+++ ToDoList/Model.swift
@@ -26,20 +26,46 @@ class ToDoItem: Codable {
 class Model {
     var items: [ToDoItem] = []
     
+    init() {
+        loadData()
+    }
+    
     func addItem(title: String) {
         items.append(ToDoItem(title: title, date: Date(), isCompleted: false))
+        saveData()
     }
     
     func deleteItem(atIndex index: Int) {
         items.remove(at: index)
+        saveData()
     }
     
     func renameItem(atIndex index: Int, newTitle: String) {
         items[index].title = newTitle
+        saveData()
         
     }
     
     func toogleItem(atIndex index: Int) {
         items[index].isCompleted.toggle()
+        saveData()
     }
+    
+    let url = FileManager.default.urls(for: .libraryDirectory, in: .userDomainMask)[0].appending(path: "database.data")
+    
+    private func saveData() {
+        print(url)
+        let data = try? JSONEncoder().encode(items)
+        try? data?.write(to: url)
+    }
+    
+    private func loadData() {
+        if let data = try? Data(contentsOf: url) {
+            items =  (try? JSONDecoder().decode([ToDoItem].self, from: data)) ?? []
+        }
+        
+       
+    }
+    
+
 }
